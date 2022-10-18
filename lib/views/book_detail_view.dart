2a0ddc4d7ea4029879e5ba9af32ff497be2fa02b
Chaerul Hadi <chaerulliez@gmail.com>
2022-10-18 09:54:49 +0700

    add View Page of Book Detail

---
+++ lib/views/book_detail_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DetailBook extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Book Detail of XYZ')
+      ),
+      body: Center(
+        child: Text('Yeaaaaaaah'),
+      ),
+    );
+  }
+
+}
